8c47ff34f1ef65eaf0292feda749b8109532c2d1
hc <hanicohe@g.jct.ac.il>
2025-06-29 17:54:19 +0300

    Update Constrain.sql

--- ex2/Constrain.sql
+++ ex2/Constrain.sql
@@ -4,5 +4,5 @@ ALTER COLUMN amount SET NOT NULL;
 ALTER TABLE tax
 ADD CONSTRAINT chk_percentage CHECK (percentage >= 0 AND percentage <= 100);
 
-ALTER TABLE invoice
-ALTER COLUMN type SET DEFAULT 'A';
+ALTER TABLE transaction
+ALTER COLUMN status SET DEFAULT 'Approved';
