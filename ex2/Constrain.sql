6a37bee6aec3dee35e3f838961be5d1ffcddbc07
hc <hanicohe@g.jct.ac.il>
2025-06-29 17:16:07 +0300

    Update Constrain.sql

--- ex2/Constrain.sql
+++ ex2/Constrain.sql
@@ -4,5 +4,5 @@ ALTER COLUMN amount SET NOT NULL;
 ALTER TABLE tax
 ADD CONSTRAINT chk_percentage CHECK (percentage >= 0 AND percentage <= 100);
 
-ALTER TABLE supplier
-ALTER COLUMN country SET DEFAULT 'Israel';
+ALTER TABLE invoice
+ALTER COLUMN type SET DEFAULT 'A';
